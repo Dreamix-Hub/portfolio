5404273d1493cc3c610432cd549c87cb04e0e035
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-04-30 17:36:38 +0500

    add backend structure with configuration, database setup, and main application logic

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+from .database import Base, engine 
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    # startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    # shutdown
+    await engine.dispose()
+    
+    
+app = FastAPI(lifespan=lifespan)
+

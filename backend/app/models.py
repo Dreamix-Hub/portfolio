c2606d18d5fb844478dae639539b40aac4039a59
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-04-30 18:11:14 +0500

    add: implement models for About, TechStack, and Education in the backend

---
+++ backend/app/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, Text ,Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+class About(Base):
+    __tablename__ = "about"
+    
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    image_link: Mapped[str] = mapped_column(Text, nullable=False)
+    
+class TechStack(Base):
+    __tablename__ = "tech_stack"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    languages: Mapped[str] = mapped_column(String(150), nullable=False)
+    framework: Mapped[str] = mapped_column(String(150), nullable=False)
+    tools: Mapped[str] = mapped_column(String(150), nullable=False)
+    databases: Mapped[str] = mapped_column(String(150), nullable=False)
+
+class Education(Base):
+    __tablename__ = "education"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(Text, nullable=False)
+    start_date: Mapped[str] = mapped_column(String(5))
+    end_date: Mapped[str] = mapped_column(String(5), default="present")

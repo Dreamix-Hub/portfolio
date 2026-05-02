35086c799d7c3485c288cbb67b15caf8d6d84575
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-05-02 19:15:23 +0500

    add blog and blog category model

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from sqlalchemy import Integer, Text, String, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from typing import List
+from datetime import datetime
+
 from .database import Base
 
 class Admin(Base):
@@ -20,12 +23,10 @@ class About(Base):
     headline: Mapped[str] = mapped_column(Text, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
     profile_image_link: Mapped[str | None] = mapped_column(Text, nullable=True)
-    email_public: Mapped[str] = mapped_column(String(150), nullable=True)
-    location: Mapped[str] = mapped_column(String(200), nullable=True)
+    email_public:Mapped[str | None] = mapped_column(String(150), nullable=True)
+    location: Mapped[str | None] = mapped_column(String(200), nullable=True)
     github_link: Mapped[str] = mapped_column(String(200))
     linkedin_link: Mapped[str] = mapped_column(String(200))
-    email: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
-    password_has: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)
   
 # association table for using in Many-to-many relationship 
 project_techstack = Table(
@@ -40,7 +41,7 @@ class ProjectCategory(Base):
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     category_name: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)    
-    project: Mapped[list["Project"]] = relationship(back_populates="project_category")  # <---- 1-to-M relationship with project
+    project: Mapped[List["Project"]] = relationship(back_populates="project_categories")  # <---- 1-to-M relationship with project
     
 class TechStack(Base):
     __tablename__ = "tech_stacks"
@@ -48,7 +49,7 @@ class TechStack(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     type: Mapped[str] = mapped_column(String(50), nullable=False)
-    projects: Mapped[list["Project"]] = relationship(
+    projects: Mapped[List["Project"]] = relationship(
         secondary="project_techstack",
         back_populates="tech_stacks"
     )
@@ -63,14 +64,38 @@ class Project(Base):
     featured: Mapped[bool | None] = mapped_column(default=False)
     
     category_id: Mapped[int] = mapped_column(
-        ForeignKey("project_category.id"),
+        ForeignKey("project_categories.id"),
         nullable=False,
         index=True
     )
     category: Mapped[ProjectCategory] = relationship(back_populates="projects") # <---- M-to-1 relationship with project_category
     
-    techstack: Mapped[list["TechStack"]] = relationship(
+    techstack: Mapped[List["TechStack"]] = relationship(
         secondary="project_techstack",
         back_populates="projects"
     )
+    
+class BlogCategory(Base):
+    __tablename__ = "blog_categories"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    blog: Mapped[List["Blog"]] = relationship(back_populates="blog_category")
+    
+class Blog(Base):
+    __tablename__ = "blogs"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] 
+    content: Mapped[str]
+    is_draft: Mapped[bool]
+    created_at: Mapped[datetime]
+    updated_at: Mapped[datetime]
+    
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("blog_category.id"),
+        nullable=False,
+        index=True
+    )
+    category: Mapped[BlogCategory] = relationship(back_populates="blogs") 
 

1022c6d3bbe509ed077c448b3ce7dfba0e66886f
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-05-02 20:00:56 +0500

    add ProjectCreate model with fields for title, description, category_id, techstack_ids, live_link, github_link, and featured status

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
+from typing import Optional
 class AdminBase(BaseModel):
     username: str = Field(min_length=4)
 
@@ -18,3 +19,13 @@ class Token:
     token_type: str
     access_token: str
     
+class ProjectCreate(BaseModel):
+
+    title: str = Field(min_length=5)
+    description = Field(min_length=10)
+    category_id: int 
+    techstack_ids: list[int]
+    live_link: Optional[str] = None
+    github_link: Optional[str] = None
+    featured: Optional[bool] = Field(default=False)
+

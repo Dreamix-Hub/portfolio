cb5f74e04c225aa6a32d6535adb8c7bfd35489e3
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-05-02 20:18:01 +0500

    refactor: update ProjectCreate and ProjectUpdate schemas for consistency and clarity

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -22,10 +22,37 @@ class Token:
 class ProjectCreate(BaseModel):
 
     title: str = Field(min_length=5)
-    description = Field(min_length=10)
-    category_id: int 
-    techstack_ids: list[int]
+    description: str = Field(min_length=10)
     live_link: Optional[str] = None
     github_link: Optional[str] = None
     featured: Optional[bool] = Field(default=False)
+    category_id: int 
+    techstack_ids: list[int]
 
+class ProjectUpdate(BaseModel):
+    title: Optional[str] = None 
+    description: Optional[str] = None 
+    live_link: Optional[str] = None
+    github_link: Optional[str] = None
+    featured: Optional[bool] = Field(default=False)
+    category_id: int 
+    techstack_ids: Optional[list[int]] = None 
+    
+class ProjectCategorySchema(BaseModel):
+    id: int
+    category_name: str
+
+class TechStackSchema(BaseModel):
+    id: int
+    name: str
+    type: str
+    
+class ProjectResponse(BaseModel):
+    id : int
+    title: str
+    description: str
+    category: ProjectCategorySchema
+    techstacks: list[TechStackSchema]
+    live_link: str
+    github_link: str
+    featured: bool
